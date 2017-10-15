7707c2a180f9179e21a3d2bfda823ab8da2a36d3
Lukas Capkovic <lcapkovic@gmail.com>
2017-10-15 18:45:47 +0100

    Implement rate limiting

--- server.py
+++ server.py
@@ -1,8 +1,15 @@
 from flask import Flask
 from flask import request
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
 import sqlite3 as sql
 
 app = Flask(__name__)
+limiter = Limiter(
+    app,
+    key_func=get_remote_address,
+    default_limits=["15 per minute", "1 per second"],
+)
 
 answers = []
 hints = []
